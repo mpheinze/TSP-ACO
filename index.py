92337998a148f69a2abab5f8303bafe6e0ceb158
mpheinze <heinze89@hotmail.com>
2019-10-12 15:01:00 +0200

    added node class and cleaned up a bit

--- index.py
+++ index.py
@@ -5,9 +5,9 @@ import pandas as pd
 from utils import distance_matrix
 from matplotlib import pyplot as plt
 
+
 # number of nodes on the map
 N_NODES = 10
-
 
 # generating random arrays of integers
 x = [random.randint(1, 20) for i in range(N_NODES)]
@@ -16,8 +16,3 @@ y = [random.randint(1, 20) for i in range(N_NODES)]
 node_list = list(zip(x, y))
 dist_matrix = distance_matrix(node_list)
 
-
-# scatter plotting nodes
-fig, ax = plt.subplots(figsize=(6, 6))
-plt.scatter(x, y, s=200, color='firebrick')
-plt.show()
---
+++ node.py
@@ -0,0 +1,5 @@
+class Node():
+    def __init__(self, i=0, x_cord, y_cord):
+        self.index = i
+        self.x = x_cord
+        self.y = y_cord
